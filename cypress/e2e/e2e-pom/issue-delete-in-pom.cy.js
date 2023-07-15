8c767c82985cc60e85a52aeabf53d1dc85b7852a
Allansarg <allansarg0@gmail.com>
2023-07-15 16:27:16 +0300

    My issue-delete-in-pom

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -7,8 +7,8 @@ describe('Issue delete', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
-    cy.contains(issueTitle).click();
+      //open issue detail modal with title from line 16  
+      cy.contains(issueTitle).click();
     });
   });
 
@@ -16,10 +16,22 @@ describe('Issue delete', () => {
   const issueTitle = 'This is an issue of type: Task.';
 
   it('Should delete issue successfully', () => {
-    //add steps to delete issue
+    //Add steps to delete issue
+    IssueModal.clickDeleteButton();
+    IssueModal.confirmDeletion();
+
+    // Assert that the issue is deleted and not displayed on the Jira board anymore
+    IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle);
+
   });
 
   it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
+    //Add steps to start deletion proces but cancel it
+    IssueModal.clickDeleteButton();
+    IssueModal.cancelDeletion();
+    IssueModal.closeDetailModal();
+
+    // Assert that the issue is not deleted and still displayed on the Jira board
+    IssueModal.ensureIssueIsVisibleOnBoard(issueTitle);
   });
 });
